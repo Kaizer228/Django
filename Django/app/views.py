cb3e8ec8d4f52dd5c9eca327edcf192645a0353f
Marc PC <martinezmarc228@gmail.com>
2024-10-07 21:01:52 +0800

    LAST PUSH

--- Django/app/views.py
+++ Django/app/views.py
@@ -5,18 +5,18 @@ from django import forms
 tasks = []  
 
 
-
-
 class AddTaskForm(forms.Form):
-    task = forms.CharField(label="",
+
+    task = forms.CharField(
+        max_length=100,
         widget=forms.TextInput(attrs={
             'class': 'w-full p-3 mb-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
-            'placeholder': 'Add your task here...'
+            'placeholder': 'Add your task here...',
         })
     )
 
     date = forms.DateField(
-        label="",
+       
         widget=forms.DateInput(attrs={
             'class': 'w-full p-3 mb-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
             'placeholder': 'Select a date',
@@ -24,10 +24,21 @@ class AddTaskForm(forms.Form):
         })
     )
 
+    priority = forms.ChoiceField(
+        choices=[('Urgent', 'Urgent'), ('Not Urgent', 'Not Urgent'), ('Not Important', 'Not Important')],
+        widget=forms.Select(attrs={
+            'class': 'w-full p-3 mb-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+        })
+    )
+
+
+ 
+
 
 
 def dashboard(request):
-  
+
+    
     if 'DELETE' in request.GET:
         task_index = int(request.GET.get('DELETE')) 
         tasks.pop(task_index)  
@@ -35,18 +46,23 @@ def dashboard(request):
 
    
     if request.method == "POST":
+        
         form = AddTaskForm(request.POST)
+
         if form.is_valid():
+
             task = form.cleaned_data["task"]
             date = form.cleaned_data["date"]
-            tasks.append({"task": task, "date": date})
+            priority = form.cleaned_data["priority"]
+
+            tasks.append({"task": task, "date": date, "priority" : priority})
             return redirect('dashboard')
 
     else:
         form = AddTaskForm()
 
     return render(request, "app/dashboard.html", {
-        'title': 'Dashboard',
+        'title': 'TODO-LIST',
         'tasks': tasks,
         'form': form
     })
